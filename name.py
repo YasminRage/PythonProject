8df06bd7b02411fd0d11d3dddff177ee1f849ce7
YasminRage <yasmin_rage@outlook.com>
2025-12-25 13:17:21 +0000

    Add files via upload

---
+++ name.py
@@ -0,0 +1,19 @@
+name = "ada lovelace"
+print(name.title())
+
+name = "Ada Lovelace"
+
+#FullName
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+
+print(full_name) # ada lovelace
+    
+    
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+# Hello, Ada Lovelace!
